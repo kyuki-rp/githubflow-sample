c6783580859f4a6778b505e2432925340b7d851a
k-yukio <k-yuki@analytics-jp.com>
2023-02-13 18:04:49 +0900

    first commit

---
+++ src/sample.py
@@ -0,0 +1,26 @@
+import datetime
+import requests
+
+
+class Chatbot():
+    def get_response(self, request):
+        greetres = GreetRes(request)
+        res = greetres.get()
+        return res
+
+
+class GreetRes():
+
+    def __init__(self, request):
+        self.request = request
+
+    def get(self):
+        self.request
+        if self.request == "おはよう":
+            return "おはようございます。"
+        elif self.request == "こんにちは":
+            return "こんにちは。"
+        elif self.request == "こんばんは":
+            return "こんばんは。"
+        else:
+            raise Exception("予期しない入力値です。")
